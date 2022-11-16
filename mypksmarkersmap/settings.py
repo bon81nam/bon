f2abd57c2c5f165b46ef706a46541e5837d07def
bon81nam <38066356+bon81nam@users.noreply.github.com>
2022-11-16 09:02:04 +0800

    Update settings.py

--- mypksmarkersmap/settings.py
+++ mypksmarkersmap/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = "mypksmarkersmap.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
+    #"default": {
         #"ENGINE": "django.db.backends.sqlite3",
         #"NAME": BASE_DIR / "db.sqlite3",
         #'ENGINE': 'django.db.backends.postgresql',
@@ -90,7 +90,7 @@ DATABASES = {
         #'PORT': '',
         #'USER': 'mohdfadli',
         #'PASSWORD': '12345678'
-    }
+    #}
 }
 
 
